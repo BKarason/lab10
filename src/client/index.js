c436eadaedc244f703e2d9f8dc294199a1ffeb54
HjorturJ <hjorturv17@ru.is>
2018-10-23 21:11:13 +0000

    got the fetch to kind of work

--- src/client/index.js
+++ src/client/index.js
@@ -1,14 +1,16 @@
 const _ = require("lodash");
 function component() {
- let element = document.createElement('div');
- /*fetch("/api/greeting/" + name)
- .then((resp) => resp.json())
- .then((data) => {
- 	console.log(data);
- })*/
+	let element = document.createElement('div');
+	var name = "O";
+	fetch("/api/greeting/" + name)
+	.then((resp) => resp.json())
+	.then((resp) => {
+		console.log(resp);
+		element.innerHTML = resp;
+	})
 
- element.innerHTML = _.join(['Hello', 'there!'], ' ');
- return element;
+	//element.innerHTML = _.join(['Hello', 'there!'], ' ');
+	return element;
 }
 document.body.appendChild(component());
 
