606620e171751ef4922bf928564ad91e520aa57f
Birkir K <birkir17@ru.is>
2018-10-23 20:40:06 +0000

    hurrrr

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,12 @@
 const _ = require("lodash");
 function component() {
  let element = document.createElement('div');
+ /*fetch("/api/greeting/" + name)
+ .then((resp) => resp.json())
+ .then((data) => {
+ 	console.log(data);
+ })*/
+
  element.innerHTML = _.join(['Hello', 'there!'], ' ');
  return element;
 }
